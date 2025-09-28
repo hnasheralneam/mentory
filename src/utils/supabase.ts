cb1e1f87da50ad4921a7065e2fc6e9674dbeb432
Hamza Nasher-Alneam <hnasheralneam@gmail.com>
2025-09-28 02:20:41 -0400

    Fix loading bar colors, page title

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://asyuffwwgteelixgoczt.supabase.co'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
-const supabase = createClient(supabaseUrl, supabaseKey || "");
+const supabase = createClient(supabaseUrl || "", supabaseKey || "");
 
 export default supabase;
